a4a32de5b452132fa7608082dd7c3a6d90a2c3c9
HaileySnow <harvey.funda07@gmail.com>
2024-09-23 18:59:51 +0800

    displayhelloworldcomponent1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { DisplayhelloworldComponent } from './displayhelloworld/displayhelloworld.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{
+    title: 'Component #1: Display Hello World',
+    component: DisplayhelloworldComponent,
+    path: 'hello-world',
+}]
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+})
+export class AppRoutingModule {}
