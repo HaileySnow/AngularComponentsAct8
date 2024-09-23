82292948accd4032c4fd5ccde0e72ce32d5208b4
HaileySnow <harvey.funda07@gmail.com>
2024-09-23 18:57:08 +0800

    first commit

---
+++ src/app/displayhelloworld/displayhelloworld.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displayhelloworld',
+  standalone: true,
+  imports: [],
+  templateUrl: './displayhelloworld.component.html',
+  styleUrl: './displayhelloworld.component.css'
+})
+export class DisplayhelloworldComponent {
+  shownow = 'Hello World';
+
+}
