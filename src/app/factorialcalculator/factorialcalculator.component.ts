c7bd83344c98745975e0bb3c904ecae113424b2c
HaileySnow <harvey.funda07@gmail.com>
2024-09-24 23:20:19 +0800

    factorialcalculatorcomponent23

---
+++ src/app/factorialcalculator/factorialcalculator.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-factorialcalculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './factorialcalculator.component.html',
+  styleUrl: './factorialcalculator.component.css'
+})
+export class FactorialcalculatorComponent {
+
+  @ViewChild('numberToBeFactorial') numberToBeFactorial!: ElementRef;
+    result = 0;
+    num = 0;
+
+    startFactorial(): void {
+        this.num = Number(this.numberToBeFactorial.nativeElement.value);
+        this.result = this.factorial(this.num);
+    }
+
+    factorial(n: number): number {
+        return n > 1 ? n * this.factorial(n - 1) : 1;
+    }
+
+}
